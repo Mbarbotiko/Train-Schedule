1642e321d78a8ef7d527c649226b3e7c78bd92f6
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-24 11:37:13 -0500

    fixed table row issue when appending rows and sequence of data being appended, only thing left to do is use moment calculations to add how far train is, and add styling to webpage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,15 +16,7 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
-    var words = "string of words user"
-    function addingRows() {
 
-        $('.table-dark>tbody:last').append("<td>" + words, "<td>" + words, "<td>" + words, "<td>" + words, "<td>" + words);
-
-
-    };
-
-    addingRows();
 
     $('#submitBtn').one('click', function (event) {
         event.preventDefault();
@@ -34,27 +26,55 @@ $(document).ready(function () {
         //find moment js info later for this entry
         var frequency = $('#frequency-input').val().trim();
 
-        var newTrain ={
-            name:trainName,
-            place:destination,
-            start:firstTrain,
-            rate:frequency
+        var newTrain = {
+            name: trainName,
+            place: destination,
+            start: firstTrain,
+            rate: frequency
         };
 
         database.ref().push(newTrain);
-        
+
         console.log(newTrain.name);
         console.log(newTrain.place);
         console.log(newTrain.start);
         console.log(newTrain.rate);
 
-        alert('you added a train');
+        $("#train-input").val("");
+        $("#destination-input").val("");
+        $("#first-train-input").val("");
+        $("#frequency-input").val("");
 
 
     });
 
+    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
-    // function to add user input into the table
+        console.log(childSnapshot.val());
+
+
+        var trainName = childSnapshot.val().name;
+        var destination = childSnapshot.val().place;
+        var firstTrain = childSnapshot.val().start;
+        var frequency = childSnapshot.val().rate;
+
+
+        console.log(trainName);
+        console.log(destination);
+        console.log(firstTrain);
+        console.log(frequency);
+
+
+        function addingRows() {
+
+            $('.table-dark>tbody:last').append("<tr><td>" + trainName+"</td><td>" + destination+"</td><td>" + frequency+"</td><td>" + firstTrain+ "</td><td>" + 'minutesaway');
+    
+    
+        };
+
+        addingRows();
+    });
+
 
 
 
