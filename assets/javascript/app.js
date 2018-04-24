daea2a20f8dbff6456642ff30e73b35382dc7791
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-24 10:42:31 -0500

    adding variabled for user input fields

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,16 +3,18 @@ $(document).ready(function () {
 
 
 
-  var config = {
-    apiKey: "AIzaSyBb1aBpu_QJSDe3ve1z4NAkDDJyp1Y7jhA",
-    authDomain: "trainschedule-1c628.firebaseapp.com",
-    databaseURL: "https://trainschedule-1c628.firebaseio.com",
-    projectId: "trainschedule-1c628",
-    storageBucket: "",
-    messagingSenderId: "562600325932"
-  };
-  firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyBb1aBpu_QJSDe3ve1z4NAkDDJyp1Y7jhA",
+        authDomain: "trainschedule-1c628.firebaseapp.com",
+        databaseURL: "https://trainschedule-1c628.firebaseio.com",
+        projectId: "trainschedule-1c628",
+        storageBucket: "",
+        messagingSenderId: "562600325932"
+    };
 
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
 
     var words = "string of words user"
     function addingRows() {
@@ -23,6 +25,17 @@ $(document).ready(function () {
     };
 
     addingRows();
+
+$('#submitBtn').one('click', function(event){
+    event.preventDefault();
+var trainName=$('#train-input').val().trim();
+var destination=$('#destination-inpu').val().trim();
+var firstTrain=$('#first-train-inpu').val().trim();
+var frequency=$('#frequency-input').val().trim();
+
+
+}
+
 
     // function to add user input into the table
 
