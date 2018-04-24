54ec4e313eaa87ef90db7ae401c5d988611da7b8
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-24 10:58:15 -0500

    added functionality for onclick button to send user data to store in firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         authDomain: "trainschedule-1c628.firebaseapp.com",
         databaseURL: "https://trainschedule-1c628.firebaseio.com",
         projectId: "trainschedule-1c628",
-        storageBucket: "",
+        storageBucket: "https//trainschedule-1c628.appspot.com",
         messagingSenderId: "562600325932"
     };
 
@@ -26,15 +26,32 @@ $(document).ready(function () {
 
     addingRows();
 
-$('#submitBtn').one('click', function(event){
-    event.preventDefault();
-var trainName=$('#train-input').val().trim();
-var destination=$('#destination-inpu').val().trim();
-var firstTrain=$('#first-train-inpu').val().trim();
-var frequency=$('#frequency-input').val().trim();
+    $('#submitBtn').one('click', function (event) {
+        event.preventDefault();
+        var trainName = $('#train-input').val().trim();
+        var destination = $('#destination-input').val().trim();
+        var firstTrain = $('#first-train-input').val().trim();
+        //find moment js info later for this entry
+        var frequency = $('#frequency-input').val().trim();
+
+        var newTrain ={
+            name:trainName,
+            place:destination,
+            start:firstTrain,
+            rate:frequency
+        };
+
+        database.ref().push(newTrain);
+        
+        console.log(newTrain.name);
+        console.log(newTrain.place);
+        console.log(newTrain.start);
+        console.log(newTrain.rate);
+
+        alert('you added a train');
 
 
-}
+    });
 
 
     // function to add user input into the table
